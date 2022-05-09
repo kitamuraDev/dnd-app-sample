e9200a110b0fe250b326e00ce27914dc070ba0ab
kitamuraDev <masato1999824@icloud.com>
2022-05-09 23:02:53 +0900

    :bento: add assets

---
+++ src/stores/cuckoos-data.ts
@@ -0,0 +1,64 @@
+const CUCKOOS = [
+  {
+    id: 'yoshi!!',
+    value: 'ヨシ!!',
+    image: '/images/1_yoshi!!.png',
+  },
+  {
+    id: 'bishi',
+    value: 'ビシッ',
+    image: '/images/2_bishi.png',
+  },
+  {
+    id: 'yatta',
+    value: 'やったー',
+    image: '/images/3_yatta.png',
+  },
+  {
+    id: 'okdesu',
+    value: 'OKです',
+    image: '/images/4_okdesu.png',
+  },
+  {
+    id: 'nanntutte',
+    value: 'なんつって！',
+    image: '/images/5_nanntutte.png',
+  },
+  {
+    id: 'suki',
+    value: 'すき',
+    image: '/images/6_suki.png',
+  },
+  {
+    id: 'ryoukai',
+    value: '了解',
+    image: '/images/7_ryoukai.png',
+  },
+  {
+    id: 'ohayougozaimasu',
+    value: 'おはようございます',
+    image: '/images/8_ohayougozaimasu.png',
+  },
+  {
+    id: 'omoshiro',
+    value: 'おもしろっ',
+    image: '/images/9_omoshiro.png',
+  },
+  {
+    id: 'unnunn',
+    value: 'うんうん',
+    image: '/images/10_unnunn.png',
+  },
+  {
+    id: 'arigatou',
+    value: 'ありがとう',
+    image: '/images/11_arigatou.png',
+  },
+  {
+    id: 'biribiri',
+    value: 'ビリビリ',
+    image: '/images/12_biribiri.png',
+  },
+];
+
+export default CUCKOOS;
