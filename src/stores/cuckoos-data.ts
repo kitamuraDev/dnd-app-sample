cbeb257838992f803f15af3cecf5c1d4502159ae
kitamuraDev <masato1999824@icloud.com>
2022-05-19 22:29:30 +0900

    :truck: imageフォルダの移動。正常に画像を表示できた #4

--- src/stores/cuckoos-data.ts
+++ src/stores/cuckoos-data.ts
@@ -1,63 +1,76 @@
+import UNNUNN from '../assets/images/10_unnunn.png';
+import ARIGATOU from '../assets/images/11_arigatou.png';
+import BIRIBIRI from '../assets/images/12_biribiri.png';
+import YOSHI from '../assets/images/1_yoshi.png';
+import BISHI from '../assets/images/2_bishi.png';
+import YATTA from '../assets/images/3_yatta.png';
+import OKDESU from '../assets/images/4_okdesu.png';
+import NANNTUTTE from '../assets/images/5_nanntutte.png';
+import SUKI from '../assets/images/6_suki.png';
+import RYOUKAI from '../assets/images/7_ryoukai.png';
+import OHAYOUGOZAIMASU from '../assets/images/8_ohayougozaimasu.png';
+import OMOSHIRO from '../assets/images/9_omoshiro.png';
+
 const CUCKOOS = [
   {
     id: 'yoshi!!',
     value: 'ヨシ!!',
-    image: '/images/1_yoshi!!.png',
+    image: YOSHI,
   },
   {
     id: 'bishi',
     value: 'ビシッ',
-    image: '/images/2_bishi.png',
+    image: BISHI,
   },
   {
     id: 'yatta',
     value: 'やったー',
-    image: '/images/3_yatta.png',
+    image: YATTA,
   },
   {
     id: 'okdesu',
     value: 'OKです',
-    image: '/images/4_okdesu.png',
+    image: OKDESU,
   },
   {
     id: 'nanntutte',
     value: 'なんつって！',
-    image: '/images/5_nanntutte.png',
+    image: NANNTUTTE,
   },
   {
     id: 'suki',
     value: 'すき',
-    image: '/images/6_suki.png',
+    image: SUKI,
   },
   {
     id: 'ryoukai',
     value: '了解',
-    image: '/images/7_ryoukai.png',
+    image: RYOUKAI,
   },
   {
     id: 'ohayougozaimasu',
     value: 'おはようございます',
-    image: '/images/8_ohayougozaimasu.png',
+    image: OHAYOUGOZAIMASU,
   },
   {
     id: 'omoshiro',
     value: 'おもしろっ',
-    image: '/images/9_omoshiro.png',
+    image: OMOSHIRO,
   },
   {
     id: 'unnunn',
     value: 'うんうん',
-    image: '/images/10_unnunn.png',
+    image: UNNUNN,
   },
   {
     id: 'arigatou',
     value: 'ありがとう',
-    image: '/images/11_arigatou.png',
+    image: ARIGATOU,
   },
   {
     id: 'biribiri',
     value: 'ビリビリ',
-    image: '/images/12_biribiri.png',
+    image: BIRIBIRI,
   },
 ];
 
